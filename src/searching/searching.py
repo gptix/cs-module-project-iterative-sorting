c3a361457fcc77a69ae23f716f68f440e9bfc609
Jud Taylor <jud.taylor@protonmail.com>
2020-06-15 19:40:10 -0400

    passed.

--- src/searching/searching.py
+++ src/searching/searching.py
@@ -1,14 +1,35 @@
 def linear_search(arr, target):
-    # Your code here
+    
+    if arr == None or target == None:
+        return None
 
+    found = -1
+    idx = -1
 
-    return -1   # not found
+    for cell in arr:
+        idx += 1
+        if cell == target:
+            found = idx
+
+    return found  # -1 if not not found
 
 
 # Write an iterative implementation of Binary Search
 def binary_search(arr, target):
 
-    # Your code here
+    left_end = 0
+    right_end = len(arr)-1
+    midpoint = 0
 
+    while left_end <= right_end:
+        midpoint = (right_end + left_end) // 2
+        if arr[midpoint] < target:
+            left_end = midpoint + 1
+
+        elif arr[midpoint] > target:
+            right_end = midpoint - 1
+
+        else:
+            return midpoint
 
     return -1  # not found
